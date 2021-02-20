99614f0101ac45fc33d2361caf21c1a7c930ba30
Soha Jiwani <sohajiwani1@gmail.com>
2021-02-20 17:15:27 -0500

    accounted for escape characters, modified readme, and added the binary executable file for those who don't have go installed

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # NameJokeGen
+
+## About
+This program is a web service written in Go hosted on <code>http://localhost:5000</code> when it is ran. 
+
+## Instructions to Run
+If you don't have Go installed on your computer, run the following command on your terminal to run the binary executable file:
+<pre><code>./namejokegen</pre></code>
+
+If you do have Go installed on your computer, run the following command on your terminal:
+<pre><code>go run .</pre></code>
+
+Once it has started running, either use your favorite browser to navigate to <code>http://localhost:5000</code> or run the following command on your terminal:
+<pre><code>curl 'http://localhost:5000'</pre></code> 
--- namejokegen.go
+++ namejokegen.go
@@ -3,66 +3,79 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
 	"net/http"
 	"os"
+	"html"
 )
 
+/* 
+	NameResponse is a struct to hold the relevant values from the https://names.mcquay.me/api/v0/ API
+*/
 type NameResponse struct {
 	FirstName string `json:"first_name"`
 	LastName string `json:"last_name"`
 }
-
-// { "type": "success", "value": { "id": 181, "joke": "John Doe's OSI network model has only one layer - Physical.", "categories": ["nerdy"] } }
+/*
+	JokeResponse is a struct to hold the needed joke from the Chuck Norris Joke API
+*/
 type JokeResponse struct {
 	Value struct {
 		Joke string `json:"joke"`
-	} `json:"value`
+	} `json:"value"`
 }
 
 
 func main()  {
-	// puts the response on the server
-	http.HandleFunc("/", genServer)
+	// puts the joke on the web server hosted on http://localhost:5000
+	http.HandleFunc("/", GenServer)
 	http.ListenAndServe(":5000", nil)
 }
 
-func genServer(w http.ResponseWriter, r *http.Request)  {
-	// Loading in the name
+/* 
+	GenServer pulls the first and last name from the first API, then inserts the first and last
+	name into the URL for the joke's API. Then, it will get a response from the joke API which
+	will be used to construct an HTTP response.
+*/
+func GenServer(w http.ResponseWriter, r *http.Request)  {
+	// HTTP GET call to the API
 	nResponse, err := http.Get("https://names.mcquay.me/api/v0/")
 
+	// Error checking
 	if err != nil {
 		fmt.Print(err.Error())
 		os.Exit(1)
 	}
 
-	nData, err := ioutil.ReadAll(nResponse.Body)
-
+	// Turning the response into a NameResponse object
+	nData, err := io.ReadAll(nResponse.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
-	
 	var name NameResponse
 	json.Unmarshal(nData, &name)
 
 	
-	// Loading in the joke
+	// HTTP GET call to the joke API
 	jokeLink := fmt.Sprintf("http://api.icndb.com/jokes/random?firstName=%s&lastName=%s&limitTo=\\[nerdy\\]", name.FirstName, name.LastName)
 	jResponse, err := http.Get(jokeLink)
 
+	// Error handling
 	if err != nil {
 		fmt.Print(err.Error())
 		os.Exit(1)
 	}
 
-	jData, err := ioutil.ReadAll(jResponse.Body)
-
+	// Turning the response into a JokeResponse object
+	jData, err := io.ReadAll(jResponse.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	var j JokeResponse
 	json.Unmarshal(jData, &j)
-	fmt.Fprintf(w, "%s", j.Value.Joke)
+
+	// This line will unescape any entities in the Joke response
+	newJoke := html.UnescapeString(j.Value.Joke)
+	fmt.Fprintf(w, "%s", newJoke)
 }
